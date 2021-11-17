3950be22fb5e859833d55a8c0791a0f4b51dbcc0
Gyong97 <999kgy@naver.com>
2021-11-18 00:18:26 +0900

    for https

--- ocr_server/app.js
+++ ocr_server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/ocr_from_get_image');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 app.all('/*', function(req, res, next) { 
   res.header("Access-Control-Allow-Origin", "*"); 
   res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
