448b1f58319c3c801562a878dddbb157317a3bbc
Gyong97 <999kgy@naver.com>
2021-11-29 03:59:34 +0900

    test

--- ocr_server/routes/ocr.js
+++ ocr_server/routes/ocr.js
@@ -114,7 +114,7 @@ router.post('/', function(req, res, next) {
             )
             return img_folder_name;
         }).then((img_folder_name)=>{ //완료한 폴더 삭제.
-            fs.rmdir(img_folder_name, {recursive: true},function(err){
+            fs.rmdir(img_folder_name, {recursive: true},err =>{
                 if(err) {
                 console.log("Error : ", err)
                 }
