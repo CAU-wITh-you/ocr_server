c179fe6c5d1df884d162f9e89a54be4133b4e4ab
Gyong97 <999kgy@naver.com>
2022-08-27 13:57:31 +0900

    connectDB

--- ocr_server/database/database.js
+++ ocr_server/database/database.js
@@ -2,7 +2,7 @@ const config = require('../config');
 const MongoDb = require('mongodb');
 
 async function connectDB(){
-    return (await MongoDb.MongoClient.connect(config.db_host)).then( (client) => client.db());
+    return MongoDb.MongoClient.connect(config.db_host).then( (client) => client.db());
 }
 
 module.exports.connectDB = connectDB;
